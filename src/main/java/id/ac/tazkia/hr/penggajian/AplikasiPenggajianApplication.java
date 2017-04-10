f4b477259eb6d925cb1d7c6b84fa99618ef06674
Endy Muhardin <endy.muhardin@gmail.com>
2017-04-10 16:15:07 +0700

    starter project

---
+++ src/main/java/id/ac/tazkia/hr/penggajian/AplikasiPenggajianApplication.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.hr.penggajian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiPenggajianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiPenggajianApplication.class, args);
+	}
+}
---
+++ src/main/resources/db/migration/V1.0.0.1__Skema Awal.sql
@@ -0,0 +1,4 @@
+create table karyawan(
+  id varchar(36),
+  PRIMARY KEY (id)
+);
